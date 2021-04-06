3e6b685f5de779db53440513655b9336e8786f88
sunnavy <sunnavy@bestpractical.com>
2021-04-07 03:24:32 +0800

    Default RT path to /opt/rt5 in docs for RT 5

--- lib/RT/Extension/CustomField/HideEmptyValues.pm
+++ lib/RT/Extension/CustomField/HideEmptyValues.pm
@@ -27,7 +27,7 @@ of Request Tracker when you view a ticket or another object.
 
 May need root permissions
 
-=item Edit your F</opt/rt4/etc/RT_SiteConfig.pm>
+=item Edit your F</opt/rt5/etc/RT_SiteConfig.pm>
 
 If you are using RT 4.2 or greater, add this line:
 
@@ -41,7 +41,7 @@ or add C<RT::Extension::CustomField::HideEmptyValues> to your existing C<@Plugin
 
 =item Clear your mason cache
 
-    rm -rf /opt/rt4/var/mason_data/obj
+    rm -rf /opt/rt5/var/mason_data/obj
 
 =item Restart your webserver
 
